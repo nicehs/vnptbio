0ecf601f8cfc26bc638a58d74c46c281ff039973
chin <58085885+nicehs@users.noreply.github.com>
2025-12-12 17:50:25 +0700

    Delete vnptbio directory

--- vnptbio/modules/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Environment name (e.g. dev, staging, prod)"
-  type        = string
-}
-
-variable "project_name" {
-  description = "Project name used as prefix for resources"
-  type        = string
-}
